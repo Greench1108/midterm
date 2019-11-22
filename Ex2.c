518102f3ddd207af8eaf2ded21f98e0466eddd32
Greench1108 <1839848600@qq.com>
2019-11-22 01:14:31 -0800

    added a Ex2.c file

---
+++ Ex2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i=0,j=0,c,b;
+    unsigned int a[10];
+    while((c=getchar())!=' ')
+    for(i=0;i<10;i++);
+    for(j=0;j<10;j++);
+    if(a[j]<a[i])
+{
+    b=a[i];
+}
+printf("The max is%d\n",b);
+return 0;
+}
+    
